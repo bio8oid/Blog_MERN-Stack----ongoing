2ce81adf0c32eec053a62de2214efea0b4073e2a
bio8oid <8bollod8@mail.co.uk>
2020-01-25 21:06:08 +0100

    Update project file structure

--- client/src/components/features/NavBar/NavBar.js
+++ client/src/components/features/NavBar/NavBar.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const links = [{ path: '/Home', title: 'Home' }, { path: '/About', title: 'About' }]
+
 class NavBar extends React.Component {
 
+  state = {
+    links: [
+      { path: '/', title: 'Home' },
+      { path: '/posts/new', title: 'Add post' },
+      { path: '/posts', title: 'Posts' },
+      { path: '/contact', title: 'Contact' },
+    ],
+  }
+    
   render() {
+    const { links } = this.state;
+
     return (
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/posts'>Posts</Link>
-        <Link to='/posts/new'>Add Post</Link>
-        <Link to='/contact'>Contact</Link>
-      </div>
+      <nav>
+        <Logo />
+        <MainMenu links={links} />
+      </nav>
     );
   }
+
+  // render() {
+  //   return (
+  //     <div>
+  //       <Link to='/'>Home</Link>
+  //       <Link to='/posts'>Posts</Link>
+  //       <Link to='/posts/new'>Add Post</Link>
+  //       <Link to='/contact'>Contact</Link>
+  //     </div>
+  //   );
+  // }
 
 }
 
