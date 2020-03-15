388ef0e5246c132c56e66da095ec25dbbec6dca9
bio8oid <8bollod8@mail.co.uk>
2020-03-16 00:15:58 +0100

    Add Posts and PostsCounter files

--- client/src/components/features/NavBar/NavBar.js
+++ client/src/components/features/NavBar/NavBar.js
@@ -5,36 +5,36 @@ const links = [{ path: '/Home', title: 'Home' }, { path: '/About', title: 'About
 
 class NavBar extends React.Component {
 
-  state = {
-    links: [
-      { path: '/', title: 'Home' },
-      { path: '/posts/new', title: 'Add post' },
-      { path: '/posts', title: 'Posts' },
-      { path: '/contact', title: 'Contact' },
-    ],
-  }
+  // state = {
+  //   links: [
+  //     { path: '/', title: 'Home' },
+  //     { path: '/posts/new', title: 'Add post' },
+  //     { path: '/posts', title: 'Posts' },
+  //     { path: '/contact', title: 'Contact' },
+  //   ],
+  // }
     
-  render() {
-    const { links } = this.state;
-
-    return (
-      <nav>
-        <Logo />
-        <MainMenu links={links} />
-      </nav>
-    );
-  }
-
   // render() {
+  //   const { links } = this.state;
+
   //   return (
-  //     <div>
-  //       <Link to='/'>Home</Link>
-  //       <Link to='/posts'>Posts</Link>
-  //       <Link to='/posts/new'>Add Post</Link>
-  //       <Link to='/contact'>Contact</Link>
-  //     </div>
+  //     <nav>
+  //       <Logo />
+  //       <MainMenu links={links} />
+  //     </nav>
   //   );
   // }
+
+  render() {
+    return (
+      <div>
+        <Link to='/'>Home</Link>
+        <Link to='/posts'>Posts</Link>
+        <Link to='/posts/new'>Add Post</Link>
+        <Link to='/contact'>Contact</Link>
+      </div>
+    );
+  }
 
 }
 
--- client/src/components/pages/Posts/PostsPage.js
+++ client/src/components/pages/Posts/PostsPage.js
@@ -1,8 +1,24 @@
+// import React from 'react';
+
+// const PostsPage = () => (
+//   <div>
+//     <h1>Posts</h1>
+//   </div>
+// );
+
+// export default PostsPage;
+
 import React from 'react';
+
+import PageTitle from '../../common/PageTitle/PageTitle';
+import PostsCounter from '../../features/PostsCounter/PostsCounter';
+import Posts from '../../features/Posts/Posts';
 
 const PostsPage = () => (
   <div>
-    <h1>Posts</h1>
+    <PageTitle>Posts list</PageTitle>
+    <PostsCounter />
+    <Posts />
   </div>
 );
 
