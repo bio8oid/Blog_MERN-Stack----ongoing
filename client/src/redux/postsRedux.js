f42220b1c17dac5a547463ccd322da5cfc99c8af
bio8oid <8bollod8@mail.co.uk>
2020-03-16 22:15:43 +0100

    Add redux-thunk and dev tools

---
+++ client/src/redux/postsRedux.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import { API_URL } from '../config';
+
+
+/* SELECTORS */
+
+export const getPosts = ({ posts }) => posts;  
+
+/* ACTIONS */
+
+// action name creator
+const reducerName = 'posts';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+export const LOAD_POSTS = createActionName('LOAD_POSTS');
+export const loadPosts = payload => ({ payload, type: LOAD_POSTS });
+
+/* INITIAL STATE */
+
+const initialState = [];
+
+/* REDUCER */
+
+export default function reducer(statePart = initialState, action = {}) {
+    switch (action.type) {
+        case LOAD_POSTS:
+            return [...action.payload];
+        default:
+            return statePart;
+    }
+};
+
+/* THUNKS */
+
+export const loadPostsRequest = () => {
+    return dispatch => {
+
+        axios.get(`${API_URL}/posts`).then(res => {
+            dispatch(loadPosts(res.data));
+        })
+            .catch(err => {
+                console.log(err.message);
+            });
+
+    };
+};
